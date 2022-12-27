a7647cd032d6f7a8f90b57063ec58ee35d2336a8
Ikhanoba Michael-Shaka <shakaikhanoba@gmail.com>
2022-12-27 23:13:35 +0100

    feat: started auth process flow on backend

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from 'helmet'
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import authRoutes from './routes/auth.js'
+import { register } from "./controllers/auth.js";
 
 
 const __filename = fileURLToPath(
@@ -34,6 +36,11 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage })
+
+app.post("/auth/register", upload.single("picture", register))
+
+
+app.use("/auth", authRoutes)
 
 
 const PORT = process.env.PORT || 6001;
